e6880da655bb1d5157cf98c25c7056a464d41941
Caleb Morris <caleb.morris.g@gmail.com>
2012-02-25 21:30:52 -0700

    created very skeleton gui

--- src/pkg360/Main.java
+++ src/pkg360/Main.java
@@ -1,19 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg360;
 
-/**
- *
- * @author cdbitesky
- */
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        guiFrame fr = new guiFrame();
+        fr.setVisible(true);
     }
 }

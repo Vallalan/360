ec62234ffefde9b1cf4065df532cf76d9e5488ba
Alan Gieske <amgieske@asu.edu>
2012-04-14 22:15:52 -0700

    Fixed a bug with reading in the clues. added some documentation.

--- src/pkg360/InputTest.java
+++ src/pkg360/InputTest.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.util.*;
 /*
  * @author Caleb Morris
+ * @author Alan Gieske
  */
 
 public class InputTest {
@@ -10,6 +11,7 @@ public class InputTest {
 	private Vector<String> wordList;
 	private int sizeOfBoard;
 
+	//enum for the direction of a word (across or down)
 	//Access from outside class with InputTest.Orientation.ACROSS or DOWN
 	public enum Orientation {
 		ACROSS, DOWN;
@@ -18,11 +20,17 @@ public class InputTest {
     public void main() {
         Vector<String> v = new Vector<String>();
         String fileName = "test_file.txt";
+
+
         try {
+
+			//Attempt to open and read specified file
             BufferedReader reader = 
                 new BufferedReader( 
                     new FileReader(fileName) );
             String line = reader.readLine();
+
+			//read in all words and enter them as individual string elements in the vector v
             while(line!=null) {
                 v.addAll(Arrays.asList((line.split(" "))));
                 System.out.println(line);
@@ -34,8 +42,13 @@ public class InputTest {
         }
         int start = -1,end;
         int t = v.size();
+
+		//interate through the entire vector searching for any comment indicators
+		//removing them and the contained comments
         for( int i = 0; i < v.size(); ++i ) {
             String tmp = v.elementAt(i);
+
+			//spots a start of comment indicator
             if( tmp.indexOf("/*") > -1 && start == -1 ) {
                 start = i;
                 System.out.println("\t/* found." + start);
@@ -54,6 +67,8 @@ public class InputTest {
 		wordList = new Vector<String>();
 
         System.out.println("=========================");
+
+		//begin iterating through the array to gather the data
         for( int i = 0; i < v.size(); ++i ) {
             String tmp = v.elementAt(i);
 
@@ -65,7 +80,7 @@ public class InputTest {
 			//to add a clue to the list
 			} else if( tmp.indexOf('\"') > -1) {
 
-				//Add a clue to the clueList
+				//Add a clue and all it's data to the clueList
 				String clue= tmp; //begin the clue string
 
 				//Finish the clue string
@@ -75,25 +90,27 @@ public class InputTest {
 						clue = clue + " " + tmp;
 				} while( tmp.indexOf('\"') == -1);
 
+				//move to the next string
+				tmp = v.elementAt(++i);
+
+
 				//Append the extra elements of the clue, delimited by '/'
 				while( tmp.indexOf('#') == -1) {
 					clue = clue + "/" + tmp;
-					++i;
-					tmp = v.elementAt(i);
+					tmp = v.elementAt(++i);
 
 				}
 				System.out.println(clue);
 				clueList.add(clue);
+
+			//Final case - words, make certain we don't include blank lines
 			} else if (!tmp.equals(""))  {
 
-				//Add a word to the wordlist
+				//Add a word and all data to the wordlist
 				String word = tmp;
-				while( tmp.indexOf('#') == -1) {
-					
-					++i;
-					tmp = v.elementAt(i);
-					word = word + "/" + tmp;
-					
+				while( tmp.indexOf('#') == -1) {										
+					tmp = v.elementAt(++i);
+					word = word + "/" + tmp;					
 				}
 				System.out.println(word);
 				wordList.add(word);
@@ -108,6 +125,7 @@ public class InputTest {
 		//test statements for getter methods
 		System.out.println(getString(wordList.elementAt(0)));
 		System.out.println(getString(clueList.elementAt(0)));
+		System.out.println(xPos(clueList.elementAt(1)) + " " + yPos(clueList.elementAt(1)));
 		System.out.println(xPos(wordList.elementAt(1)) + " " + yPos(wordList.elementAt(1)));
 		System.out.println(getLength(wordList.elementAt(0)));
 		if(getOrientation(wordList.elementAt(0)) == Orientation.ACROSS) {
@@ -116,11 +134,14 @@ public class InputTest {
 		if(getOrientation(clueList.elementAt(4)) == Orientation.DOWN) {
 			System.out.println("Down is good");
 		}
+
         System.out.println("=========================");
     }
 
 
-	//All the fun getter methods
+	//All the getter methods
+
+	//gets the string value given a clue or word from one of the Lists
 	public String getString(String hint) {
 		String value = hint.split("/")[0];
 		if (value.startsWith("\"")) {
@@ -131,20 +152,24 @@ public class InputTest {
 	}
 
 
+	//returns the x position of the word or clue
 	public int xPos(String hint) {
 		int pos = Integer.parseInt(hint.split("/")[1]);
 		return (int)Math.floor(pos/sizeOfBoard);
 	}
 
+	//returns the y position of the word or clue
 	public int yPos(String hint) {
 		int pos = Integer.parseInt(hint.split("/")[1]);
 		return (int)Math.IEEEremainder(pos, sizeOfBoard);
 	}
 
+	//returns the length of the word or clue
 	public int getLength(String hint) {
 		return Integer.parseInt(hint.split("/")[2]);
 	}
 
+	//returns the orientation of the word or clue
 	public Orientation getOrientation(String hint) {
 		String dir = hint.split("/")[3];
 
@@ -156,18 +181,22 @@ public class InputTest {
 
 	}
 
+	//returns the clueList
 	public Vector<String> getClues() {
 		return clueList;
 	}
 
+	//returns the wordList
 	public Vector<String> getWords() {
 		return wordList;
 	}
 
+	//returns the BoardSize
 	public int getBoardSize() {
 		return sizeOfBoard;
 	}
 
+	//utility method to check if a String represents an integer.
 	private boolean isParsableToInt(String z) {
 		try {
 			Integer.parseInt(z);
@@ -178,7 +207,7 @@ public class InputTest {
 	}
 	//Given the vector and the starting element of a comment
 	//removes everything included in the comment bounds.
-	public void ClearComment(Vector<String> v, int start) {
+	private void ClearComment(Vector<String> v, int start) {
 		for( int z = start; z < v.size(); ++z ) {
 			String tmp = v.elementAt(z);
 
